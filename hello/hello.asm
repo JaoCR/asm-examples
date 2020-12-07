51afca2b58513d4fe124c77402de1c561d983250
jc.rodrigues1997@usp.com <jc.rodrigues1997@usp.com>
2020-12-07 12:09:51 -0300

    ENH: beutified hello world example

--- hello/hello.asm
+++ hello/hello.asm
@@ -1,21 +1,50 @@
+;     _   ___ _____ 
+;    (_) /   |  _  |
+;     _ / /| | |/' |
+;    | / /_| |  /| |
+;    | \___  \ |_/ /
+;    | |   |_/\___/ 
+;   _/ |            
+;  |__/             
+;
+;  j40     : 11399628#USP
+;  made_in : Sao_Carlos, SP, BR, 2020_12
+;
+;  --- x86_64 nasm assembly: Hello World! ---
+
+; >> This program writes "Hello, World!" to
+; stdout.
+
+
 global _start
+
 
 section .text
 
 _start:
 
-  mov rax, 1
-  mov rdi, 1
-  mov rsi, hello
-  mov rdx, 13
-  syscall
+    ; write message to stdout 
+    mov rax, SYS_WRITE
+    mov rdi, FD_STDOUT 
+    mov rsi, str_msg 
+    mov rdx, len_msg
+    syscall
 
-  mov rax, 60
-  xor rdi, rdi
-  syscall
+    ; exit program
+    mov rax, SYS_EXIT
+    xor rdi, rdi
+    syscall
 
 
 section .data
 
-hello:
-  db "Hello, World", 10
+    ;syscalls
+    SYS_WRITE:  equ 1
+    SYS_EXIT:   equ 60
+
+    ;file descriptor
+    FD_STDOUT:  equ 1
+
+    ; the message
+    str_msg:    db "Hello, World!", 10
+    len_msg:    equ $ - str_msg 
